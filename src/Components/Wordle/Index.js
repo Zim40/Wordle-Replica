f5aaa5f90ad13adb2479a1c444645a3a2491361c
User <michaelm810129@gmail.com>
2023-08-04 21:33:55 +0930

    added rules/about sections while handling useState

--- src/Components/Wordle/Index.js
+++ src/Components/Wordle/Index.js
@@ -1,14 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Wordle() {
   // Worlde "GAME" and Container
+
+  const [descriptionVisible, setDescriptionVisible] = useState(false);
+  const [rulesVisible, setRulesVisible] = useState(false);
+
+  const showRules = () => {
+    setRulesVisible(!rulesVisible);
+    setDescriptionVisible(descriptionVisible);
+  };
+
+  const showAbout = () => {
+    setDescriptionVisible(!descriptionVisible);
+    setRulesVisible(rulesVisible);
+  };
+
   return (
     <div className="game-Container">
-      <h4 style={{fontWeight: 'bold'}}>About: </h4>
-        <p>
-          Unleash your inner wordsmith with Wordle! In this captivating word
-          puzzle game, decipher the 5-letter mystery word in just six attempts.
-        </p>
+      <button
+        type="button"
+        name="about"
+        className="aboutBtn"
+        onClick={showAbout}
+      >
+        About{" "}
+      </button>
+
+      <button
+        type="button"
+        name="about"
+        className="rulesBtn"
+        onClick={showRules}
+      >
+        Rules{" "}
+      </button>
+      <div className="game--abt-rules">
+        {descriptionVisible && (
+          <div>
+            {" "}
+            <p
+              id="description"
+              style={{
+                fontWeight: 600,
+                zIndex: 1,
+                visibility: descriptionVisible ? "visible" : "hidden",
+              }}
+            >
+              Unleash your inner wordsmith with Wordle! In this captivating word
+              puzzle game, decipher the 5-letter mystery word in just six
+              attempts.
+            </p>
+          </div>
+        )}
+        {rulesVisible && (
+          <div>
+            <ol
+              id="rules"
+              style={{
+                fontWeight: 600,
+                zIndex: 2,
+                visibility: rulesVisible ? "visible" : "hidden",
+              }}
+            >
+              <li>Objective: Guess a five-letter word within six attempts.</li>
+              <li>Guessing: Make valid English word guesses.</li>
+              <li>Feedback: Colors indicate correct letters and positions.</li>
+              <li>Limited Attempts: Six guesses allowed.</li>
+              <li>Winning: Guess the word correctly to win.</li>
+            </ol>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
