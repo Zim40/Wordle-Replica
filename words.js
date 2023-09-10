b9e14aec4debe49cb6047e40d5e06249ff57b2f5
User <michaelm810129@gmail.com>
2023-09-10 16:06:13 +0930

    Updates

---
+++ words.js
@@ -0,0 +1,16 @@
+// window.onload= function () {
+//     function fetchWords() {
+//         fetch('https://api.datamuse.com/words?sp=?????&max=50')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//         }
+        
+//         )
+//         .catch(error => {
+//             console.Log(error);
+//         })
+//     }
+//     fetchWords();
+    
+// }
